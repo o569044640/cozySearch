ddc48ec3a074ea3109f6523db46884185f5b570b
Jarvis Ou <jarvis.ou@construction.com>
2019-02-15 17:15:23 -0800

    Clean up code

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -7,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
+  siteListFirstRow = [
+    {name: 'Facebook', iconPath: '../../assets/table-icons/facebook.png', websiteURL: 'https://www.facebook.com' },
+    {name: 'Amazon', iconPath: '../../assets/table-icons/amazon.png', websiteURL: 'https://www.amazon.com' },
+    {name: 'Blogger', iconPath: '../../assets/table-icons/blogger.png', websiteURL: 'https://www.Blogger.com'  },
+    {name: 'Google', iconPath: '../../assets/table-icons/google.png', websiteURL: 'https://www.google.com' },
+    {name: 'Gmail', iconPath: '../../assets/table-icons/gmail.png', websiteURL: 'https://www.google.com/gmail/' },
+  ];
+
+  siteListSecondRow = [
+    {name: 'Instagram', iconPath: '../../assets/table-icons/instagram.png', websiteURL: 'https://www.instagram.com' },
+    {name: 'Amazon', iconPath: '../../assets/table-icons/amazon.png', websiteURL: 'https://www.amazon.com' },
+    {name: 'Blogger', iconPath: '../../assets/table-icons/blogger.png', websiteURL: 'https://www.Blogger.com'  },
+    {name: 'Google', iconPath: '../../assets/table-icons/google.png', websiteURL: 'https://www.google.com' },
+    {name: 'Gmail', iconPath: '../../assets/table-icons/gmail.png', websiteURL: 'https://www.google.com/gmail/' },
+  ];
+
+  siteListThirdRow = [
+    {name: 'Facebook', iconPath: '../../assets/table-icons/facebook.png', websiteURL: 'https://www.facebook.com' },
+    {name: 'Amazon', iconPath: '../../assets/table-icons/amazon.png', websiteURL: 'https://www.amazon.com' },
+    {name: 'Blogger', iconPath: '../../assets/table-icons/blogger.png', websiteURL: 'https://www.Blogger.com'  },
+    {name: 'Google', iconPath: '../../assets/table-icons/google.png', websiteURL: 'https://www.google.com' },
+    {name: 'Gmail', iconPath: '../../assets/table-icons/gmail.png', websiteURL: 'https://www.google.com/gmail/' },
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
